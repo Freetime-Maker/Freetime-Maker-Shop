9a3b806f875dfb25f686ed12b528f09522f0fd2e
Freetime Maker <jamieachatz@gmail.com>
2026-03-05 18:00:05 +0100

    c

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve static files from the root directory
-app.use(express.static(path.join(__dirname, '..')));
+// Serve static files from the current directory
+app.use(express.static(__dirname));
 
 // For any other route, serve index.html (SPA fallback)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 module.exports = app;
