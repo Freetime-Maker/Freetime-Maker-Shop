e92e82caed11c9ba601d7433463cd6db167876b6
Freetime Maker <jamieachatz@gmail.com>
2026-03-06 19:26:18 +0100

    added

--- main.js
+++ main.js
@@ -22,12 +22,4 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// For local testing
-if (require.main === module) {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
 module.exports = app;
